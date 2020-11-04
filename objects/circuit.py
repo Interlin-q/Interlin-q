ba6a96aec1b4d70824db170090c8d99a2066106b
Rhea Parekh <rheaparekh12@gmail.com>
2020-11-04 19:24:07 +0530

    Add Circuit class stencil

---
+++ objects/circuit.py
@@ -0,0 +1,68 @@
+class Circuit(object):
+    """
+    Circuit object which contains information about a quantum circuit.
+    """
+
+    def __init__(self, q_map, layers=[]):
+        """
+        Returns the important things for a quantum circuit
+
+        Args:
+            q_map (dict): A mapping of the qubits required for the circuit to the
+               computing host ID where the qubit is located
+            layers (list): List of layers, where each layer contains a collection
+               of gates to be applied on the qubits in the system
+        """
+
+        self._q_map = q_map
+        self._layers = layers
+
+    @property
+    def q_map(self):
+        """
+        Get the *q_map* of the circuit
+        Returns:
+            (dict): A mapping of the qubits required for the circuit to the
+                computing host ID where the qubit is located
+        """
+        return self._q_map
+
+    @property
+    def layers(self):
+        """
+        Get the *layers* of the circuit
+        Returns:
+            (list): List of layers, where each layer contains a collection of
+                gates to be applied on the qubits in the system
+        """
+        return self._layers
+
+    def total_qubits(self):
+        """
+        Get the total number of qubits in the circuit
+        Returns:
+            (int): total number of qubits in circuit
+        """
+
+        return len(self._q_map)
+
+    def add_new_qubit(qubit_info):
+        """
+        Add a new qubit to the circuit
+        Args:
+            qubit_info (dict): Map of new qubit ID to the computing host ID
+        """
+
+        if list(qubit_info.keys())[0] in self._q_map:
+            raise ValueError("Qubit already added")
+        self._q_map.update(qubit_info)
+
+    def add_layer_to_circuit(self, layer):
+        """
+        Add a new layer for the circuit
+
+        Args:
+            layer (list): List of collection of gates to be applied to the system
+        """
+        
+        self._layers.append(layer)
