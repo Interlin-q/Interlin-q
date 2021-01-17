469929f2e6c29f78de485ca5827954c81ae44d44
Rhea Parekh <rheaparekh12@gmail.com>
2021-01-17 17:01:49 +0530

    Update README and add quick example template

--- examples/template.py
+++ examples/template.py
@@ -1,42 +1,25 @@
-[![Unitary Fund](https://img.shields.io/badge/Supported%20By-UNITARY%20FUND-brightgreen.svg?style=for-the-badge)](http://unitary.fund)
+import sys
+sys.path.append("../")
 
-# Interlin-q
+from qunetsim.components import Network
+from qunetsim.objects import Logger
 
-This is a distributed quantum-enabled simulator which imitates the master-slave centralised-control distributed quantum computing system with interconnect communication between the nodes. Using this simulator, one can input any monolithic circuit as well as a distributed quantum computing topology and see the execution of the distributed algorithm happening in real time. The simulator maps the monolithic circuit to a distributed quantum computing achitecture and uses a master-slave relation with a centralized controller communicating to computing nodes in the network. Interlin-q can be used for designing and analysing novel distributed quantum algorithms for various distributed quantum computing architectures.
+from interlinq import (ControllerHost, Constants, Clock,
+Circuit, Layer, ComputingHost, Operation)
 
-The method used to accomplish this is based on the following paper:
+import numpy as np
 
-```
-Distributed Quantum Computing and Network Control for Accelerated VQE
-Stephen DiAdamo, Marco Ghibaudi, James Cruise (arXiv: 2101.02504)
-```
+Logger.DISABLED = True
 
-The simulated architecture can be seen in the image below:
 
-![Simulated Architecture](images/simulated_architechture.png)
-
-## Setup and tests
-
-To setup Interlin-q, run the below command.
-
-```
-pip install -r requirements.txt
-```
-
-To run tests, run the below command.
-
-```
-nose2
-```
-
-## Quick Example
-
-A basic template of a distributed CNOT gate between two different computing hosts is shown in the file ```examples/template.py```, where the control qubit in first computing host is set in |1> state and the target qubit in the second computing host is set in |0> state.
-
-```
 def create_circuit(q_map):
+    """
+    Create the necessary circuit here
+    """
     layers = []
     computing_host_ids = list(q_map.keys())
+
+    # Circuit input should be added
 
     # Prepare the qubits on both computing hosts
     ops = []
@@ -46,9 +29,9 @@ def create_circuit(q_map):
             qids=q_map[host_id],
             computing_host_ids=[host_id])
         ops.append(op)
+
     layers.append(Layer(ops))
 
-    # Circuit input should be added below
     # Put qubit "q_0_0" in |1> state
     op = Operation(
         name=Constants.SINGLE,
@@ -56,6 +39,7 @@ def create_circuit(q_map):
         gate=Operation.X,
         computing_host_ids=[computing_host_ids[0]])
     ops.append(op)
+
     layers.append(Layer(ops))
 
     # Apply cnot gate from "q_0_0" to "q_1_0"
@@ -83,10 +67,12 @@ def create_circuit(q_map):
     circuit = Circuit(q_map, layers)
     return circuit
 
+
 def controller_host_protocol(host, q_map):
     """
     Protocol for the controller host
     """
+
     circuit = create_circuit(q_map)
     host.generate_and_send_schedules(circuit)
     host.receive_results()
@@ -100,24 +86,29 @@ def controller_host_protocol(host, q_map):
         for bit_id, bit in bits.items():
             print("{0}: {1}".format(bit_id, bit))
 
+
 def computing_host_protocol(host):
     """
     Protocol for the computing hosts
     """
+
     host.receive_schedule()
     host.send_results()
+
 
 def main():
     # initialize network
     network = Network.get_instance()
     network.delay = 0
     network.start()
+
     clock = Clock()
 
-    controller_host = ControllerHost(host_id="host_1", clock=clock)
+    controller_host = ControllerHost(
+        host_id="host_1",
+        clock=clock,
+    )
 
-    # Here the number of computing hosts and number of qubits per computing hosts
-    # can be customised
     computing_hosts, q_map = controller_host.create_distributed_network(
         num_computing_hosts=2,
         num_qubits_per_host=2)
@@ -134,13 +125,11 @@ def main():
         t = host.run_protocol(computing_host_protocol)
         threads.append(t)
 
-    for thread in threads:
+    for thread in threads:    
         thread.join()
     network.stop(True)
     exit()
 
+
 if __name__ == '__main__':
     main()
-```
-
-In the template, the monolothic circuit can be added in the `create_circuit` function, which will automatically be distributed by the controller host and performed by the computing hosts. The operations needed to perform the monolithic circuit should be added layer by layer. The specific topology required to perform the circuit can be customised by changing the number of computing hosts required to perform the circuit and as well as changing the number of qubits required per computing hosts.
